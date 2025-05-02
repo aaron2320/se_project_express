fff2d31330b11b6774610963d98c53d33bc22951
aaron2320 <aaron2320@gmail.com>
2025-05-02 11:21:18 -0600

    Passed Postman Test 12

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,15 +1,9 @@
-const {
-  BAD_REQUEST,
-  NOT_FOUND,
-  SERVER_ERROR,
-  FORBIDDEN,
-} = require("../utils/errors");
-
+const { SERVER_ERROR } = require("../utils/errors");
 const ClothingItem = require("../models/clothingItem");
 
 // GET /items
-const getItems = (req, res) => {
-  return ClothingItem.find({})
+const getItems = (req, res) =>
+  ClothingItem.find({})
     .then((items) => res.status(200).send(items))
     .catch((err) => {
       console.error(err);
@@ -17,9 +11,22 @@ const getItems = (req, res) => {
         .status(SERVER_ERROR)
         .send({ message: "An error occurred on the server" });
     });
-};
 
-// (rest of file unchanged)
+const createItem = (req, res) => {
+  /* your implementation */
+};
+const getItem = (req, res) => {
+  /* your implementation */
+};
+const addLike = (req, res) => {
+  /* your implementation */
+};
+const removeLike = (req, res) => {
+  /* your implementation */
+};
+const deleteItem = (req, res) => {
+  /* your implementation */
+};
 
 module.exports = {
   getItems,
--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,6 @@ const {
   BAD_REQUEST,
   NOT_FOUND,
   SERVER_ERROR,
-  UNAUTHORIZED,
   CONFLICT,
 } = require("../utils/errors");
 
@@ -12,61 +11,43 @@ const User = require("../models/user");
 const { JWT_SECRET } = require("../utils/config");
 
 // POST /signup
-const createUser = (req, res) => {
-  const { name, avatar, email, password } = req.body;
-
-  return bcrypt
-    .hash(password, 10)
-    .then((hash) => User.create({ name, avatar, email, password: hash }))
-    .then((user) => {
-      const { password: hashedPassword, ...userWithoutPassword } =
-        user.toObject();
-      return res.status(201).send(userWithoutPassword);
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        return res.status(BAD_REQUEST).send({ message: err.message });
-      }
-      if (err.code === 11000) {
-        return res.status(CONFLICT).send({ message: "User already exists" });
-      }
-      return res
-        .status(SERVER_ERROR)
-        .send({ message: "An error occurred on the server" });
-    });
-};
+const createUser = (req, res) => bcrypt
+  .hash(password, 10)
+  .then((hash) => User.create({ name, avatar, email, password: hash }))
+  .then((user) => {
+    const { password: hashedPassword, ...userWithoutPassword } = user.toObject();
+    return res.status(201).send(userWithoutPassword);
+  })
+  .catch((err) => {
+    console.error(err);
+    if (err.name === "ValidationError") {
+      return res.status(BAD_REQUEST).send({ message: err.message });
+    }
+    if (err.code === 11000) {
+      return res.status(CONFLICT).send({ message: "User already exists" });
+    }
+    return res.status(SERVER_ERROR).send({ message: "An error occurred on the server" });
+  });
 
 // POST /users
 const createUserPublic = (req, res) => {
   const { name, avatar } = req.body;
 
   if (!name || name.length < 2 || name.length > 30) {
-    return res.status(BAD_REQUEST).send({
-      message: "Name must be between 2 and 30 characters",
-    });
+    return res.status(BAD_REQUEST).send({ message: "Name must be between 2 and 30 characters" });
   }
 
   const urlRegex = /^https?:\/\/[^\s/$.?#].[^\s]*$/;
   if (!avatar || !urlRegex.test(avatar)) {
-    return res.status(BAD_REQUEST).send({
-      message: "Invalid or missing avatar URL",
-    });
+    return res.status(BAD_REQUEST).send({ message: "Invalid or missing avatar URL" });
   }
 
   const dummyEmail = `guest${Date.now()}@test.com`;
   const dummyPassword = "$2a$10$abcdefghijklmnopqrstuv1234567890ab";
 
-  return User.create({
-    name,
-    avatar,
-    email: dummyEmail,
-    password: dummyPassword,
-  })
+  return User.create({ name, avatar, email: dummyEmail, password: dummyPassword })
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
-        expiresIn: "7d",
-      });
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "7d" });
       const { password, ...userWithoutPassword } = user.toObject();
       return res.status(201).send({ ...userWithoutPassword, token });
     })
@@ -83,22 +64,19 @@ const createUserPublic = (req, res) => {
 };
 
 // GET /users
-const getUsers = (req, res) => {
-  return User.find({})
-    .then((users) => {
-      const cleanedUsers = users.map((u) => {
-        const { password, ...rest } = u.toObject();
-        return rest;
-      });
-      return res.status(200).send(cleanedUsers);
-    })
-    .catch((err) => {
-      console.error(err);
-      return res.status(SERVER_ERROR).send({ message: "Server error" });
+const getUsers = (req, res) => User.find({})
+  .then((users) => {
+    const cleanedUsers = users.map((u) => {
+      const { password, ...rest } = u.toObject();
+      return rest;
     });
-};
+    return res.status(200).send(cleanedUsers);
+  })
+  .catch((err) => {
+    console.error(err);
+    return res.status(SERVER_ERROR).send({ message: "Server error" });
+  });
 
-// GET /users/:id
 const getUserById = (req, res) => {
   const { id } = req.params;
   return User.findById(id)
@@ -116,16 +94,11 @@ const getUserById = (req, res) => {
       }
       return res.status(SERVER_ERROR).send({ message: "Server error" });
     });
-};
-
-// (rest of file unchanged)
 
 module.exports = {
   createUser,
   createUserPublic,
   getUsers,
   getUserById,
-  getCurrentUser,
-  updateUser,
-  login,
 };
+
