b4b60d512edc599959fd816aa6e51d9464781831
aaron2320 <aaron2320@gmail.com>
2025-05-02 12:21:44 -0600

    Passed Postman Test 12

--- controllers/users.js
+++ controllers/users.js
@@ -15,7 +15,8 @@ const createUser = (req, res) => {
     .hash(password, 10)
     .then((hash) => User.create({ name, avatar, email, password: hash }))
     .then((user) => {
-      const { password: hashedPassword, ...userWithoutPassword } = user.toObject();
+      const { password: hashedPassword, ...userWithoutPassword } =
+        user.toObject();
       return res.status(201).send(userWithoutPassword);
     })
     .catch((err) => {
@@ -26,25 +27,28 @@ const createUser = (req, res) => {
       if (err.code === 11000) {
         return res.status(CONFLICT).send({ message: "User already exists" });
       }
-      return res.status(SERVER_ERROR).send({ message: "An error occurred on the server" });
+      return res
+        .status(SERVER_ERROR)
+        .send({ message: "An error occurred on the server" });
     });
 };
 
 // GET /users
-const getUsers = (req, res) =>
-  User.find({})
-    .then((users) =>
-      res.status(200).send(
+const getUsers = (req, res) => {
+  return User.find({})
+    .then((users) => {
+      return res.status(200).send(
         users.map((u) => {
           const { password, ...rest } = u.toObject();
           return rest;
         })
-      )
-    )
+      );
+    })
     .catch((err) => {
       console.error(err);
       return res.status(SERVER_ERROR).send({ message: "Server error" });
     });
+};
 
 // GET /users/:id
 const getUserById = (req, res) => {
@@ -64,6 +68,7 @@ const getUserById = (req, res) => {
       }
       return res.status(SERVER_ERROR).send({ message: "Server error" });
     });
+};
 
 // Public test handler
 const createUserPublic = (req, res) => {
@@ -77,4 +82,3 @@ module.exports = {
   getUserById,
   createUserPublic,
 };
-
