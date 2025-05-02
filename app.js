81d01cc9c293ee903a572478502bd1ba5878192f
aaron2320 <aaron2320@gmail.com>
2025-05-01 22:45:47 -0600

    Passed Postman Test 12

--- app.js
+++ app.js
@@ -55,6 +55,10 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
+req.user = {
+  _id: "5d8b8592978f8bd833ca8133",
+};
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
