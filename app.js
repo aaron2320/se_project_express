122a1132dc75040c9263f6fb9657d22b724f5f3c
aaron2320 <aaron2320@gmail.com>
2025-05-02 23:11:05 -0600

    Passed Postman Test 12

--- app.js
+++ app.js
@@ -18,16 +18,18 @@ app.use((req, res, next) => {
   next();
 });
 
-// ✅ mount routers at /users and /items
 app.use("/users", usersRouter);
 app.use("/items", itemsRouter);
 
-// ✅ handle unknown routes
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => console.log("Connected to DB"))
+  .catch(console.error);
+
 app.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Requested resource not found" });
 });
 
-// ✅ centralized error handler
 app.use((err, req, res, next) => {
   console.error(err);
   const { statusCode = 500, message } = err;
@@ -37,13 +39,4 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-// ✅ connect to MongoDB and start the server
-mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
-  .then(() => {
-    console.log("Connected to DB");
-    app.listen(3001, () => {
-      console.log("Server is listening on port 3001");
-    });
-  })
-  .catch(console.error);
+module.exports = app;
