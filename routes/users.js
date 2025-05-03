1943df492584d0c7d8f74dd3e07931f1db26d3bb
aaron2320 <aaron2320@gmail.com>
2025-05-02 23:14:13 -0600

    Passed Postman Test 12

--- routes/users.js
+++ routes/users.js
@@ -3,26 +3,61 @@ const User = require("../models/user");
 
 const router = express.Router();
 
-const getUsers = (req, res) => {
-  User.find({})
+// Create user (private)
+router.post("/", (req, res) => {
+  const { name, avatar } = req.body;
+
+  if (!name || name.length < 2 || name.length > 30) {
+    return res
+      .status(400)
+      .json({ message: "Name must be between 2 and 30 characters" });
+  }
+
+  if (!avatar || typeof avatar !== "string") {
+    return res
+      .status(400)
+      .json({ message: "Avatar must be a valid URL string" });
+  }
+
+  return User.create({ name, avatar })
+    .then((user) => {
+      res
+        .status(201)
+        .json({ _id: user._id, name: user.name, avatar: user.avatar });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: "Internal server error", error: err.message });
+    });
+});
+
+// GET all users
+router.get("/", (req, res) => {
+  return User.find({})
     .then((users) => res.json(users))
     .catch((err) =>
       res
         .status(500)
         .json({ message: "Internal server error", error: err.message })
     );
-};
+});
 
-const createUser = (req, res) => {
-  const { name, avatar } = req.body;
-  User.create({ name, avatar })
-    .then((user) => res.status(201).json(user))
+// GET user by ID
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  return User.findById(id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      return res.json(user);
+    })
     .catch((err) =>
-      res.status(400).json({ message: "Bad request", error: err.message })
+      res
+        .status(500)
+        .json({ message: "Internal server error", error: err.message })
     );
-};
-
-router.get("/users", getUsers);
-router.post("/users", createUser);
+});
 
 module.exports = router;
