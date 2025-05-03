b05dbb5fc8f9908847b0a25a5cd535a6d1b07641
aaron2320 <aaron2320@gmail.com>
2025-05-02 22:47:03 -0600

    Passed Postman Test 12

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
-const User = require("../models/user"); // your mongoose User model
+const express = require("express");
+const User = require("../models/user");
+
+const router = express.Router();
 
 // Create user (private)
-const createUser = (req, res) => {
+router.post("/", (req, res) => {
   const { name, avatar } = req.body;
 
   if (!name || name.length < 2 || name.length > 30) {
@@ -16,7 +19,7 @@ const createUser = (req, res) => {
       .json({ message: "Avatar must be a valid URL string" });
   }
 
-  User.create({ name, avatar })
+  return User.create({ name, avatar })
     .then((user) => {
       res
         .status(201)
@@ -27,66 +30,34 @@ const createUser = (req, res) => {
         .status(500)
         .json({ message: "Internal server error", error: err.message });
     });
-};
-
-// Create user (public route variant)
-const createUserPublic = (req, res) => {
-  const { name, avatar } = req.body;
-
-  if (!name || name.length < 2 || name.length > 30) {
-    return res
-      .status(400)
-      .json({ message: "Name must be between 2 and 30 characters" });
-  }
-
-  if (!avatar || typeof avatar !== "string") {
-    return res
-      .status(400)
-      .json({ message: "Avatar must be a valid URL string" });
-  }
-
-  User.create({ name, avatar })
-    .then((user) => {
-      res
-        .status(201)
-        .json({ _id: user._id, name: user.name, avatar: user.avatar });
-    })
-    .catch((err) => {
-      res
-        .status(500)
-        .json({ message: "Internal server error", error: err.message });
-    });
-};
+});
 
 // GET all users
-const getUsers = (req, res) => {
-  User.find({})
+router.get("/", (req, res) => {
+  return User.find({})
     .then((users) => res.json(users))
     .catch((err) =>
       res
         .status(500)
         .json({ message: "Internal server error", error: err.message })
     );
-};
+});
 
 // GET user by ID
-const getUserById = (req, res) => {
+router.get("/:id", (req, res) => {
   const { id } = req.params;
-  User.findById(id)
+  return User.findById(id)
     .then((user) => {
-      if (!user) return res.status(404).json({ message: "User not found" });
-      res.json(user);
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      return res.json(user);
     })
     .catch((err) =>
       res
         .status(500)
         .json({ message: "Internal server error", error: err.message })
     );
-};
+});
 
-module.exports = {
-  createUser,
-  getUsers,
-  getUserById,
-  createUserPublic,
-};
+module.exports = router;
