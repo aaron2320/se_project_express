8738ad5d91ea5244c74e9ab454bd9442d9e355bd
aaron2320 <aaron2320@gmail.com>
2025-05-02 21:53:42 -0600

    Passed Postman Test 12

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-// Import routers
 const itemsRouter = require("./routes/clothingItems");
 const usersRouter = require("./routes/users");
+const signupRouter = require("./routes/signup"); // ✅ new
 
-const { createUser, login, createUserPublic } = require("./controllers/users");
 const { NOT_FOUND } = require("./utils/errors");
 
 const app = express();
@@ -15,35 +14,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// ✅ GitHub Action middleware
 app.use((req, res, next) => {
   req.user = { _id: "5d8b8592978f8bd833ca8133" };
   next();
 });
 
-// ✅ Conditional signup route
-const signupHandler =
-  process.env.NODE_ENV === "test" ? createUserPublic : createUser;
-app.post("/signup", signupHandler);
-app.post("/signin", login);
-
-// Routes
+app.use("/signup", signupRouter); // ✅ new
 app.use("/users", usersRouter);
 app.use("/items", itemsRouter);
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
-  .then(() => {
-    console.log("Connected to DB");
-  })
+  .then(() => console.log("Connected to DB"))
   .catch(console.error);
 
-// 404 handler
 app.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Requested resource not found" });
 });
 
-// Central error handler
 app.use((err, req, res, next) => {
   console.error(err);
   const { statusCode = 500, message } = err;
