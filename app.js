ebd96c14cb8a0cb00ec6841c72147460b7bfaa6c
aaron2320 <aaron2320@gmail.com>
2025-05-02 21:44:02 -0600

    Passed Postman Test 12

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-// Import routers
 const itemsRouter = require("./routes/clothingItems");
 const usersRouter = require("./routes/users");
 
-const { createUser, login, createUserPublic } = require("./controllers/users");
+const { createUser, createUserPublic } = require("./controllers/users"); // ✅ FIXED: removed login
 const { NOT_FOUND } = require("./utils/errors");
 
 const app = express();
@@ -15,19 +14,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// ✅ GitHub Action middleware
 app.use((req, res, next) => {
   req.user = { _id: "5d8b8592978f8bd833ca8133" };
   next();
 });
 
-// ✅ Conditional signup route
 const signupHandler =
   process.env.NODE_ENV === "test" ? createUserPublic : createUser;
 app.post("/signup", signupHandler);
-app.post("/signin", login);
 
-// Routes
 app.use("/users", usersRouter);
 app.use("/items", itemsRouter);
 
@@ -38,12 +33,10 @@ mongoose
   })
   .catch(console.error);
 
-// 404 handler
 app.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Requested resource not found" });
 });
 
-// Central error handler
 app.use((err, req, res, next) => {
   console.error(err);
   const { statusCode = 500, message } = err;
