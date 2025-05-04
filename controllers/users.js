e8ada27fa5baba30ef7eb2d02f9e1e859e20f38a
aaron2320 <aaron2320@gmail.com>
2025-05-04 16:55:54 -0600

    Passed Postman Test 12

--- controllers/users.js
+++ controllers/users.js
@@ -1,82 +1,42 @@
-const bcrypt = require("bcryptjs");
 const User = require("../models/user");
-const {
-  BAD_REQUEST,
-  NOT_FOUND,
-  SERVER_ERROR,
-  CONFLICT,
-} = require("../utils/errors");
+const { BAD_REQUEST, NOT_FOUND, SERVER_ERROR } = require("../utils/errors");
 
-// POST /signup
+// POST /users
 const createUser = (req, res) => {
-  const { name, avatar, email, password } = req.body;
-
-  const createUserData = { name, avatar };
-
-  if (email) {
-    createUserData.email = email;
-  }
+  const { name, avatar } = req.body;
 
   const handleError = (err) => {
     console.error(err);
     if (err.name === "ValidationError") {
       return res.status(BAD_REQUEST).send({ message: err.message });
     }
-    if (err.code === 11000) {
-      return res.status(CONFLICT).send({ message: "User already exists" });
-    }
     return res
       .status(SERVER_ERROR)
       .send({ message: "An error occurred on the server" });
   };
 
-  const sendUser = (user) => {
-    const { password: hashedPassword, ...userWithoutPassword } =
-      user.toObject();
-    return res.status(201).send(userWithoutPassword);
-  };
-
-  if (password) {
-    return bcrypt
-      .hash(password, 10)
-      .then((hash) => {
-        createUserData.password = hash;
-        return User.create(createUserData);
-      })
-      .then(sendUser)
-      .catch(handleError);
-  }
-
-  return User.create(createUserData).then(sendUser).catch(handleError);
+  return User.create({ name, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch(handleError);
 };
 
-// POST /signin
-const login = (req, res) =>
-  res.status(200).send({ message: "Login successful (placeholder)" });
-
 // GET /users
-const getUsers = (req, res) => User.find({})
-    .then((users) =>
-      res.status(200).send(
-        users.map((u) => {
-          const { password, ...rest } = u.toObject();
-          return rest;
-        })
-      )
-    )
+const getUsers = (req, res) =>
+  User.find({})
+    .then((users) => res.status(200).send(users))
     .catch((err) => {
       console.error(err);
       return res.status(SERVER_ERROR).send({ message: "Server error" });
     });
 
 // GET /users/:id
-const getUserById = (req, res) => User.findById(req.params.id)
+const getUserById = (req, res) =>
+  User.findById(req.params.id)
     .then((user) => {
       if (!user) {
         return res.status(NOT_FOUND).send({ message: "User not found" });
       }
-      const { password, ...userWithoutPassword } = user.toObject();
-      return res.status(200).send(userWithoutPassword);
+      return res.status(200).send(user);
     })
     .catch((err) => {
       console.error(err);
@@ -88,7 +48,6 @@ const getUserById = (req, res) => User.findById(req.params.id)
 
 module.exports = {
   createUser,
-  login,
   getUsers,
   getUserById,
 };
--- models/user.js
+++ models/user.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 2, maxlength: 30 },
@@ -9,45 +7,11 @@ const userSchema = new mongoose.Schema({
     required: [true, "The avatar field is required."],
     validate: {
       validator(value) {
-        return validator.isURL(value);
+        return /^https?:\/\/[^\s/$.?#].[^\s]*$/.test(value);
       },
       message: "You must enter a valid URL",
     },
   },
-  email: {
-    type: String,
-    required: false,
-    default: () => `guest${Date.now()}@test.com`,
-    validate: {
-      validator: validator.isEmail,
-      message: "You must enter a valid email",
-    },
-  },
-  password: {
-    type: String,
-    required: false,
-    select: false,
-    default: "$2a$10$abcdefghijklmnopqrstuv1234567890ab", // dummy bcrypt hash
-  },
 });
-
-userSchema.statics.findUserByCredentials = function findUserByCredentials(
-  email,
-  password
-) {
-  return this.findOne({ email })
-    .select("+password")
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new Error("Incorrect email or password"));
-      }
-      return bcrypt.compare(password, user.password).then((matched) => {
-        if (!matched) {
-          return Promise.reject(new Error("Incorrect email or password"));
-        }
-        return user;
-      });
-    });
-};
 
 module.exports = mongoose.model("user", userSchema);
