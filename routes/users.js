549d5612ecc281533b88d3f7e7716f8a0cdcf25b
Aaron Brown <aaron2320@gmail.com>
2025-06-01 04:13:39 +0000

    Add Celebrate validation to PATCH /users/me route

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
+const { celebrate, Joi } = require("celebrate");
 const { getCurrentUser, updateUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
 
 router.get("/me", auth, getCurrentUser);
-router.patch("/me", auth, updateUser);
+
+router.patch(
+  "/me",
+  auth,
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30).optional(),
+      avatar: Joi.string().uri({ scheme: ["http", "https"] }).optional(),
+    }),
+  }),
+  updateUser
+);
 
 module.exports = router;
